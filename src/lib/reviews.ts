e6a58798ab7a7cf83d632afb775ec43f39af5660
verdulife <verdu@live.com>
2023-12-07 17:53:52 +0100

    def services 3

---
+++ src/lib/reviews.ts
@@ -0,0 +1,29 @@
+import type { Item } from "@/lib/types";
+
+export const reviews: Array<Item> = [
+  {
+    avatar: "/avatars/alba-ramirez.png",
+    name: "Alba Ramírez",
+    review: `Úrsula es la veterinaria de referencia de todos los perros de la familia. Una profesional excelente que tiene toda nuestra confianza ya que ha tratado en circunstancias de diferente gravedad siempre con éxito. Nunca deja de formarse y el trato es muy humano y cercano, la recomendamos a todo el mundo!`,
+  },
+  {
+    avatar: "/avatars/pol-argemi.png",
+    name: "Pol Argemí",
+    review: `¡Excelente servicio el de Úrsula y todo su equipo! Siempre atentas a todo y con un trato excepcional. Muestran mucha preocupación y todavía más amor por los animales. Cuando llevo a mis mascotas, se siente como si no hubiera salido de casa. ¡Son unas cracks!`,
+  },
+  {
+    avatar: "/avatars/raquel-bergua.png",
+    name: "Raquel Bergua",
+    review: `Un encanto de centro, por el trato , la profesionalidad y el precio. Llevo a mis 2 mascotas, perro y gato. Tanto Laura como Úrsula son excepcionales. Te explican todo súper bien, y tratan con un cariño a los animales que está claro que tienen una profesión vocacional. La zona no está mal para aparcar, hay un área de casitas delante en la que siempre encuentro un hueco.`,
+  },
+  {
+    avatar: "/avatars/txell-borras-capdevila.png",
+    name: "Txell Borràs Capdevila",
+    review: `Úrsula ha sido nuestra veterinaria desde siempre, y con esfuerzo y dedicación ha conseguido abrir una clínica que trata a nuestras mascotas con el amor y cariño que necesitan, asimismo estableciendo una relación muy cercana y empática con los propietarios. Su maravilloso trabajo salvó la vida a nuestro perro en más de una ocasión y estaremos eternamente agradecidos. Una clínica increíble que recomendaría a todo el mundo.`,
+  },
+  {
+    avatar: "/avatars/monica-sanchez.png",
+    name: "Mónica Sánchez",
+    review: `Llevamos a Bach desde hace casi 2 años y estamos encantados con Laura y Úrsula por su profesionalidad. Diagnósticos precisos y sin sobre-medicación para nuestros peludos . De hecho bastantes  de nuestros amigos llevamos todos allí a los perretes y somos de Cunit (+20 min. De coche)`,
+  },
+];
