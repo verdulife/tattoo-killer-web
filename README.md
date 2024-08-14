46c6b545f96e7ae6ab5285d589eda4c7d4158381
verdulife <verdu@live.com>
2024-08-15 01:25:49 +0200

    hasta paginas de servicios

--- README.md
+++ README.md
@@ -8,9 +8,9 @@
   - Eliminación de hongos en uñas
   - Peeling con carbon activo
 - [x] Añadir antes de reviews nueva serccion de resultados (posts de instagram, buscar emoji para descartar post).
-- [ ] Reacer seccion contacto/mapa.
+- [x] Reacer seccion contacto/mapa.
 - [x] Revisar si se puede borrar la seccion de legal.
-- [ ] Añadir simbolor de ® en logotipo.
+- [x] Añadir simbolor de ® en logotipo.
 - [ ] Crear pagina de cada servicio.
 - [ ] Crear pagina equipo.
 - [ ] Crear pagina preguntas frequentes con esta info: https://n1laser.com/faqs/
--- src/lib/reviews.ts
+++ src/lib/reviews.ts
@@ -2,28 +2,28 @@ import type { Item } from "@/lib/types";
 
 export const reviews: Array<Item> = [
   {
-    avatar: "/avatars/alba-ramirez.png",
-    name: "Alba Ramírez",
-    review: `Úrsula es la veterinaria de referencia de todos los perros de la familia. Una profesional excelente que tiene toda nuestra confianza ya que ha tratado en circunstancias de diferente gravedad siempre con éxito. Nunca deja de formarse y el trato es muy humano y cercano, la recomendamos a todo el mundo!`,
+    avatar: "/avatars/claudia-ayora-alonso.png",
+    name: "Claudia Ayora Alonso",
+    review: "Aitor es un súper profesional, desde la sesión uno pude ver resultados habia probado otro centros sin tener éxito así que fue increíble ver el progreso con Aitor, estoy súper agradecida por hacer desaparecer mis tatuajes! Lo súper recomiendo. Muchas gracias",
   },
   {
-    avatar: "/avatars/pol-argemi.png",
-    name: "Pol Argemí",
-    review: `¡Excelente servicio el de Úrsula y todo su equipo! Siempre atentas a todo y con un trato excepcional. Muestran mucha preocupación y todavía más amor por los animales. Cuando llevo a mis mascotas, se siente como si no hubiera salido de casa. ¡Son unas cracks!`,
+    avatar: "/avatars/jose-maria-villaverde-mayor.png",
+    name: "Jose Maria Villaverde Mayor",
+    review: "Muy profesional y efectivo. No nos vamos a engañar, el proceso duele, pero merece mucho la pena por los resultados. El trato con Aitor genial, siempre dispuesto y amable. Totalmente recomendado",
   },
   {
-    avatar: "/avatars/raquel-bergua.png",
-    name: "Raquel Bergua",
-    review: `Un encanto de centro, por el trato , la profesionalidad y el precio. Llevo a mis 2 mascotas, perro y gato. Tanto Laura como Úrsula son excepcionales. Te explican todo súper bien, y tratan con un cariño a los animales que está claro que tienen una profesión vocacional. La zona no está mal para aparcar, hay un área de casitas delante en la que siempre encuentro un hueco.`,
+    avatar: "/avatars/rebeca-mendez.png",
+    name: "Rebeca Mendez",
+    review: "Trato excelente y profesionalidad 100% . Le di muchas vueltas antes de contactar con el ya que la eliminacion de tatuajes es algo complicado y no puedes ir a cualquier sitio. Despues de 3 sesiones (teniendo un cover ya hecho)pude por fin hacer un cover definitivo.Super contenta! Lo recomiendo!",
   },
   {
-    avatar: "/avatars/txell-borras-capdevila.png",
-    name: "Txell Borràs Capdevila",
-    review: `Úrsula ha sido nuestra veterinaria desde siempre, y con esfuerzo y dedicación ha conseguido abrir una clínica que trata a nuestras mascotas con el amor y cariño que necesitan, asimismo estableciendo una relación muy cercana y empática con los propietarios. Su maravilloso trabajo salvó la vida a nuestro perro en más de una ocasión y estaremos eternamente agradecidos. Una clínica increíble que recomendaría a todo el mundo.`,
+    avatar: "/avatars/jone-u-r.png",
+    name: "Jone U.R.",
+    review: "Genial! Super feliz con el resultado. Buen trabajo, un maravilloso profesional. Lo recomiendo!",
   },
   {
-    avatar: "/avatars/monica-sanchez.png",
-    name: "Mónica Sánchez",
-    review: `Llevamos a Bach desde hace casi 2 años y estamos encantados con Laura y Úrsula por su profesionalidad. Diagnósticos precisos y sin sobre-medicación para nuestros peludos . De hecho bastantes  de nuestros amigos llevamos todos allí a los perretes y somos de Cunit (+20 min. De coche)`,
+    avatar: "/avatars/tamara-laguna.png",
+    name: "Tamara Laguna",
+    review: "Gracias a su trabajo he podido eliminarme un tatuaje y me ha permitido tatuarme lo que realmente quería. Trabajo rápido y eficaz! Feliz por los resultados. Muchas gracias Aitor.",
   },
 ];
